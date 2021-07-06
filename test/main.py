6a39d43664b8a505234561b00cbc993319ff9d70
dileepdevops <dileepugrangi@gmail.com>
2021-07-06 12:26:24 +0530

    2 added change

--- test/main.py
+++ test/main.py
@@ -3,8 +3,22 @@ print('this is an exercise1')
 print('this is an exercise1')
 print('this is an exercise1')
 
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
 # check if flag is True
 if flag:
     print(num, "is not a prime number")
 else:
     print(num, "is a prime number")
+
