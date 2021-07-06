13152ae8f15cf8f38ac397f4bc9a91c49468377e
dileepdevops <dileepugrangi@gmail.com>
2021-07-06 12:25:03 +0530

    1 added change

--- test/main.py
+++ test/main.py
@@ -2,3 +2,9 @@ print('this is an exercise')
 print('this is an exercise1')
 print('this is an exercise1')
 print('this is an exercise1')
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number")
