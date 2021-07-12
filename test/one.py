ee4918daf693863e1be244c95b545fa37128e97a
dileepugrangi <ugra.deep@gmail.com>
2021-07-12 12:56:01 +0530

    Added string length

--- test/one.py
+++ test/one.py
@@ -1,1 +1,14 @@
-print('dileep is the best')
+# This is for printing
+print('This is a test')
+# This is for finding the length of a string
+s  = 'Amazing'
+print(len(s))
+# This for finding the length of hippopotomus
+s='hippopotomous'
+print(len(s))
+# This is for finding the length of string
+print('Practice makes man perfect')
+# finding the length of your name
+s = 'dileep'
+print(len(s))
+it
